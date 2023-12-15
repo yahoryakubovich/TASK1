c19fc4e8fbc4e363e92c04157dad2b71a38085ef
Yahor Yakubovich <126192444+yahoryakubovich@users.noreply.github.com>
2023-12-15 12:25:56 +0300

    Working version of the script has been created

--- main.py
+++ main.py
@@ -1,54 +1,94 @@
 import json
 import psycopg2
+from psycopg2 import sql
 
+# Замените на свои данные для подключения к базе данных
+DB_HOST = 'localhost'
+DB_PORT = '5432'
+DB_NAME = 'dormitory'
+DB_USER = 'postgres'
+DB_PASSWORD = '12345678'
 
+# Функция для чтения данных из JSON файла
 def read_json(file_path):
-    try:
-        with open(file_path, 'r') as file:
-            data = json.load(file)
-            return data
-    except FileNotFoundError:
-        print(f"File not found: {file_path}")
-    except json.JSONDecodeError:
-        print(f"Error decoding JSON in file: {file_path}")
-    return None
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    return data
 
+# Функция для подключения к базе данных
+def connect_to_database():
+    connection = psycopg2.connect(
+        host=DB_HOST,
+        port=DB_PORT,
+        database=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD
+    )
+    return connection
 
-def write_to_postgres(data_list, connection_params):
-    try:
-        connection = psycopg2.connect(**connection_params)
-        cursor = connection.cursor()
-        for data in data_list:
-            id_as_int = int(data['id'])
+# Функция для создания таблицы Rooms
+def create_rooms_table(connection):
+    with connection.cursor() as cursor:
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS rooms (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(255) NOT NULL
+            );
+        """)
+    connection.commit()
 
-            cursor.execute("""
+# Функция для создания таблицы Students
+def create_students_table(connection):
+    with connection.cursor() as cursor:
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS students (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(255) NOT NULL,
+                birthday DATE NOT NULL,
+                sex CHAR(1) NOT NULL,
+                room_id INT REFERENCES rooms(id)
+            );
+        """)
+    connection.commit()
+
+# Функция для вставки данных в таблицу Rooms
+def insert_rooms_data(connection, rooms_data):
+    with connection.cursor() as cursor:
+        for room in rooms_data:
+            cursor.execute(sql.SQL("""
                 INSERT INTO rooms (id, name)
-                VALUES (%s, %s)
-            """, (id_as_int, data['name']))
+                VALUES (%s, %s);
+            """), (room['id'], room['name']))
+    connection.commit()
 
-        connection.commit()
-        print("Data successfully written to PostgreSQL!")
+# Функция для вставки данных в таблицу Students
+def insert_students_data(connection, students_data):
+    with connection.cursor() as cursor:
+        for student in students_data:
+            cursor.execute(sql.SQL("""
+                INSERT INTO students (id, name, birthday, sex, room_id)
+                VALUES (%s, %s, %s, %s, %s);
+            """), (student['id'], student['name'], student['birthday'], student['sex'], student['room']))
+    connection.commit()
 
-    except psycopg2.Error as e:
-        print(f"Error writing to PostgreSQL: {e}")
+if __name__ == "__main__":
+    rooms_file_path = 'rooms.json'
+    students_file_path = 'students.json'
 
-    finally:
-        if connection:
-            cursor.close()
-            connection.close()
+    # Чтение данных из JSON файлов
+    rooms_data = read_json(rooms_file_path)
+    students_data = read_json(students_file_path)
 
+    # Подключение к базе данных
+    connection = connect_to_database()
 
-# Замените 'example.json' на путь к вашему JSON файлу
-json_data = read_json('rooms.json')
+    # Создание таблиц
+    create_rooms_table(connection)
+    create_students_table(connection)
 
-if json_data:
-    # Замените параметры подключения на свои
-    connection_params = {
-        'host': 'localhost',
-        'database': 'dormitory',
-        'user': 'postgres',
-        'password': '12345678',
-        'port': '5432',
-    }
+    # Вставка данных в таблицы
+    insert_rooms_data(connection, rooms_data)
+    insert_students_data(connection, students_data)
 
-    write_to_postgres(json_data, connection_params)
+    # Закрытие подключения
+    connection.close()
