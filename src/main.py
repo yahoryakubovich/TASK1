979dc7904ec3bff2652f87d98f2e596b371edabb
yahoryakubovich <yahor.yakubovich@gmail.com>
2023-12-18 13:22:50 +0100

    + query

--- src/main.py
+++ src/main.py
@@ -1,45 +1,3 @@
-from data_readers.json_data_reader import JsonDataReader
-from database.database_connector import DatabaseConnector
-from database.database_manager import DatabaseManager
-
-DB_HOST = 'localhost'
-DB_PORT = 5432
-DB_NAME = 'dormitory'
-DB_USER = 'postgres'
-DB_PASSWORD = '831212'
-
-# if __name__ == "__main__":
-#     rooms_file_path = '/home/yahoryakubovich/Desktop/TASK1/rooms.json'
-#     students_file_path = '/home/yahoryakubovich/Desktop/TASK1/students.json'
-#
-#     db_connector = DatabaseConnector(DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD)
-#     db_connector.connect()
-#
-#     database_manager = DatabaseManager(db_connector.connection)
-#
-#     database_manager.create_rooms_table()
-#     database_manager.create_students_table()
-#
-#     rooms_data = JsonDataReader.read(rooms_file_path)
-#     students_data = JsonDataReader.read(students_file_path)
-#
-#     # database_manager.insert_rooms_data(rooms_data)
-#     # database_manager.insert_students_data(students_data)
-#
-#     data_exporter = DataExporterForRooms(db_connector.connection)
-#
-#     data_exporter.export_to_format('rooms', 'exported_data_rooms.json', 'json')
-#
-#     data_exporter.export_to_format('rooms', 'exported_data_rooms.xml', 'xml')
-#
-#     data_exporter = DataExporterForStudents(db_connector.connection)
-#
-#     data_exporter.export_to_format('students', 'exported_data_students.json', 'json')
-#
-#     data_exporter.export_to_format('students', 'exported_data_students.xml', 'xml')
-#
-#     db_connector.close()
-
 import argparse
 from pathlib import Path
 from data_readers.json_data_reader import JsonDataReader
@@ -47,6 +5,12 @@ from database.database_connector import DatabaseConnector
 from database.database_manager import DatabaseManager
 from data_exporters.data_exporter_json import DataExporterJson
 from data_exporters.data_exporter_xml import DataExporterXml
+
+DB_HOST = 'localhost'
+DB_PORT = 5432
+DB_NAME = 'dormitory'
+DB_USER = 'postgres'
+DB_PASSWORD = '831212'
 
 
 def process_data(students_file_path, rooms_file_path, output_format):
